7b43527d3269694a52276e5eedbd381a2979d7c4
Matthewokiemute <ego.okiemute@gmail.com>
2024-09-06 15:10:54 +0100

    fix: fixed hero font size

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className="container pb-16 pt-10 sm:pt-16 md:pt-16">
       <div className="relative mx-auto w-full flex flex-col justify-center items-center">
-        <div className="text-5xl sm:text-7xl md:text-[220px] lg:text-[280px] font-bold uppercase tracking-wide text-center font-baseneue w-full leading-none">
+        <div className="text-5xl sm:text-7xl md:text-[220px] lg:text-[310px] font-bold uppercase tracking-wide text-center font-baseneue w-full leading-none">
           Imagine More
         </div>
         <div className="flex flex-col items-center text-center md:text-start md:items-start mt-8 max-w-lg md:absolute md:top-1/2 md:right-0 md:translate-y-[50%] md:translate-x-[-32%]">
