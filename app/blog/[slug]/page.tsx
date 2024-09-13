611009f1947902c1b50aed82edbab7da45a96563
Matthewokiemute <ego.okiemute@gmail.com>
2024-09-13 16:12:30 +0100

    feat: single blog page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,5 +1,4 @@
-
-'use client'
+"use client";
 import Button from "@/components/ui/Button";
 // import { client } from "@/sanity/lib/client";
 // import { getDetailPost } from "@/sanity/queries/posts";
@@ -10,52 +9,54 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 // import { getDetailRecent } from "@/sanity/queries/recents";
 
-
-const WorkDetail =  ({ params }: { params: { slug: string, recent: boolean } }) => {
+const WorkDetail = ({
+  params,
+}: {
+  params: { slug: string; recent: boolean };
+}) => {
   const { slug } = params;
 
-  console.log(slug)
+  console.log(slug);
 
   // const work = await getDetailRecent(slug) || await getDetailPost(slug);
   // console.log(work);
   // console.log(work.title);
 
   return (
-    <section className="pt-10 mt-24 mx-auto px-2 max-w-8xl">
-        {/* Hero Section */}
-        <div className="flex flex-col items-start justify-start text-start">
-          <div className="relative w-full flex flex-col items-start gap-8 md:gap-10 justify-start max-w-7xl mt-4">
-            <h1 className="text-6xl sm:text-7xl scale-125 sm:scale-95 md:text-[160px] font-baseneue flex flex-col items-start text-start uppercase whitespace-nowrap">
-              <span>Still in</span>
-              <span>Development.</span>
-            </h1>
-            <p className="max-w-xs text-xl sm:text-2xl text-start font-neuehaaslight md:hidden">
-              Explore the narratives that shape our work and reveal the
-              inspiration behind our projects.
-            </p>
-            <div className="max-w-3xl w-full px-2">
-              <div>
-                <label className="flex items-center justify-between w-full p-4 md:p-6 rounded-full border transition-colors duration-300 ease-in-out bg-transparent border-gray-400">
-                  <input
-                    type="text"
-                    name="subscribe"
-                    className="bg-transparent border-none focus:border-none outline-none w-full h-full py-6 placeholder:font-neuehaaslight placeholder:text-xl"
-                    placeholder="Enter email here"
-                  />
-                  <Button
-                    text="Subscribe"
-                    className="px-10 py-5 rounded-full font-normal text-base sm:text-lg md:text-[18px] flex justify-center"
-                    variant="outline"
-                  />
-                </label>
-              </div>
+    <section className="pt-10 mt-24 mx-auto px-2 max-w-8xl w-full">
+      {/* Hero Section */}
+      <div className="container flex flex-col items-start justify-start text-start mb-20 mx-auto w-full">
+        <div className="flex flex-col items-start justify-start max-w-7xl px-4 md:px-0 w-full">
+          <div className="flex items-center gap-3 py-5 text-gray-400 text-sm text-nowrap">
+            <p>BRANDING</p>
+            <span className="w-1 h-1 bg-gray-300 rounded-full"></span>
+            <p>AUGUST 14, 2024</p>
+            <span className="w-1 h-1 bg-gray-300 rounded-full"></span>
+            <p>8 MINS READ</p>
+          </div>
+          <div className="w-full text-5xl md:text-[120px] font-baseneue font-bold flex gap-4 items-center justify-start text-start uppercase whitespace-nowrap">
+            <span>Why South Circle?</span>
+            <div className="p-2 md:p-5 rounded-full border-[1px] border-black w-fit -rotate-45">
+              <Image
+                src="/assets/arrow-down.svg"
+                width={32}
+                height={32}
+                alt="arrow-down"
+                className="object-contain w-6 h-6 md:w-10 md:h-10 animate-bounce rotate-6"
+              />
             </div>
-            {/* <p className="absolute max-w-xs text-2xl text-start font-neuehaaslight right-32 sm:right-24 md:right-32 lg:right-64 bottom-48 hidden md:inline-block">
-              Explore the narratives that shape our work and reveal the
-              inspiration behind our projects
-            </p> */}
           </div>
         </div>
+        <div className="w-full py-8">
+          <Image
+            src="/assets/blogs/imagine-more.png"
+            width={1000}
+            height={200}
+            alt="imagine more"
+            className="object-contain w-[100vw]"
+          />
+        </div>
+      </div>
     </section>
   );
 };
